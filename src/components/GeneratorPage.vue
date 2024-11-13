e571dee857233331186df29895a7af2f9b26301a
Charifx7 <charif.edu7@gmail.com>
2024-11-13 16:02:53 +0700

    initial commit

---
+++ src/components/GeneratorPage.vue
@@ -0,0 +1,22 @@
+
+<template>
+    <h1>Test</h1>
+</template>
+
+<script setup>
+
+import {ref, onMouted} from 'vue'
+
+const foods = ref([
+    {
+        food:"ข้าวผัด"
+    },
+    {
+        food:"ไก่ทอด"
+    }
+
+
+])
+
+const current
+</script>
